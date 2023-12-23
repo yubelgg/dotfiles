5f3cc8db7919e9c316b90cc628d26b7082193670
yubelgg <yulongwangbusiness@gmail.com>
2023-12-23 02:16:28 -0500

    added snippets and autocomplete

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -4,7 +4,7 @@ return {
   config = function()
     local config = require("nvim-treesitter.configs")
     config.setup({
-      ensure_installed = { "lua", },
+      ensure_installed = { "lua", "html", "javascript", "cpp" },
       hightlight = { enable = true },
       indent = { enable = true },
     })
