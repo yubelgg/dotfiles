f05a1e7767e2082d80b24dc0315ff79d8513577e
yubelgg <yulongwangbusiness@gmail.com>
2023-12-22 22:20:22 -0500

    updated option and added undotree

--- nvim/lua/option.lua
+++ nvim/lua/option.lua
@@ -2,17 +2,31 @@ vim.g.mapleader = " "
 vim.g.autoformat = true
 
 local opt = vim.opt
-opt.clipboard = "unnamedplus"
-opt.cursorline = true
-opt.expandtab = true
-opt.list = true
-opt.mouse = ""
 opt.number = true
 opt.relativenumber = true
-opt.scrolloff = 4
+opt.guicursor = ""
+
+opt.clipboard = "unnamedplus"
+opt.cursorline = true
+
 opt.shiftwidth = 2
+opt.softtabstop = 2
 opt.tabstop = 2
+opt.expandtab = true
+opt.smartindent = true
+
+opt.swapfile = false
+opt.backup = false
+opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+opt.undofile = true
+
+opt.hlsearch = false
+opt.list = true
+opt.mouse = ""
+opt.scrolloff = 8
 opt.termguicolors = true
+opt.updatetime = 50
+opt.colorcolumn = "80"
 
 -- Fix markdown indentation settings
 vim.g.markdown_recommended_style = 0
