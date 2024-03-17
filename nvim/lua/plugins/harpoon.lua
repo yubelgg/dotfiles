7a747506e00091004a7c3e998121b3158c79edcb
yubelgg <yulongwangbusiness@gmail.com>
2024-03-17 03:48:34 -0400

    i3 update

---
+++ nvim/lua/plugins/harpoon.lua
@@ -0,0 +1,25 @@
+return {
+	"ThePrimeagen/harpoon",
+	branch = "harpoon2",
+	requires = { "nvim-lua/plenary.nvim" },
+	config = function()
+		local harpoon = require("harpoon")
+		harpoon.setup()
+		vim.keymap.set("n", "<leader>a", function()
+			harpoon:list():append()
+		end)
+		vim.keymap.set("n", "<C-e>", function()
+			harpoon.ui:toggle_quick_menu(harpoon:list())
+		end)
+
+		vim.keymap.set("n", "<C-b>", function()
+			harpoon:list():select(1)
+		end)
+		vim.keymap.set("n", "<C-n>", function()
+			harpoon:list():select(2)
+		end)
+		vim.keymap.set("n", "<C-m>", function()
+			harpoon:list():select(3)
+		end)
+	end,
+}
